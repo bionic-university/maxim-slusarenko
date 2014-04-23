c3964f76e41b76517ca45b920b05963059a70c6c
Maks Sliusarenko <lenindynam@gmail.com>
2014-04-23 11:40:14 +0300

    Made change +code

--- exercise_1.php
+++ exercise_1.php
@@ -1,3 +1,9 @@
 <?php
 
+$str = "nf fhf sdgfs gj j jfhgdgfgbv b hgdhgbv";
+
+foreach (count_chars($str, 1) as $i => $val) {
+   echo "\"", chr($i) , "\" встречается в строке $val раз(а).\n";
+}
+
 ?>
